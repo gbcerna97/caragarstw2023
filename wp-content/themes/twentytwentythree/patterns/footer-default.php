465be278128a8038f27615593d1bbdce47e5844e
Gerald Cerna <gbcerna1997@gmail.com>
2023-09-18 09:29:54 +0800

    Layout changes

--- wp-content/themes/twentytwentythree/patterns/footer-default.php
+++ wp-content/themes/twentytwentythree/patterns/footer-default.php
@@ -6,13 +6,10 @@
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
-		<!-- wp:site-title {"level":0} /-->
+	<div class="wp-block-group">
 		<!-- wp:paragraph {"align":"right"} -->
-		<p class="has-text-align-right">
+		<p class="has-text-align-center">
 		<?php
 		printf(
 			/* Translators: WordPress link. */
